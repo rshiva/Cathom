edf3235206fa904ade94582a0d9ccbe4fc4e179f
shiva <nadarshiva.r@gmail.com>
2014-05-09 19:25:59 +0530

    fixed static folder for images

--- flaskr/views.py
+++ flaskr/views.py
@@ -10,7 +10,7 @@ import os
 
 
 # This is the path to the upload directory
-app.config['UPLOAD_FOLDER'] = '/tmp/'
+app.config['UPLOAD_FOLDER'] = '/flaskr/static/uploads'
 # These are the extension that we are accepting to be uploaded
 app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg', 'gif'])
 
@@ -25,12 +25,12 @@ def index():
 	entries = Entry.query.all()
 	return render_template('index.html',entries=entries)
 
-@app.route('/upload',methods=['POST'])
-def upload():
+@app.route('/create',methods=['POST'])
+def create():
 	file = request.files['file']
 	if file and allowed_file(file.filename):
 	  filename = secure_filename(file.filename)
-	  file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+	  file.save(os.getcwd() + os.path.join(app.config['UPLOAD_FOLDER'], filename))
 	entry=Entry(request.values['title'],request.values['content'],file.filename)
 	db.session.add(entry)
 	db.session.commit()  
@@ -38,7 +38,7 @@ def upload():
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-  return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
+  return send_from_directory(os.getcwd()+app.config['UPLOAD_FOLDER'],filename)
 
 
 @app.route('/new',methods=['GET'])
